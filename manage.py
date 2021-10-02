8c01716ec7c234f943edd8f86aa9bf08e6cb4cda
Sushil Kanathia <67565900+sushilkanathia@users.noreply.github.com>
2021-10-03 00:14:29 +0530

    comment
    It's very important to write the basics of the library that you used in your program.

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-import os
-import sys
+
+import os    #os provides the functions for interacting with the operating system.
+import sys   # system-specific parameter and functions
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pydjango_ci_integration.settings")
@@ -11,7 +12,7 @@ if __name__ == "__main__":
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
         try:
-            import django
+            import django    #Django is the high-level python framework for creating a secure and maintainable websites.
         except ImportError:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
